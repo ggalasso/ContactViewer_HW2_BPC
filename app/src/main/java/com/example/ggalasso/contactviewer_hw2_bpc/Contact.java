d24265373e73789808d2189e3ca241ab2bdbcb57
ggalasso <ggalasso@gmail.com>
2015-02-17 19:55:19 -0600

    First Update

---
+++ app/src/main/java/com/example/ggalasso/contactviewer_hw2_bpc/Contact.java
@@ -0,0 +1,68 @@
+package com.example.ggalasso.contactviewer_hw2_bpc;
+
+/**
+ * Created by ggalasso on 2/13/15.
+ */
+public class Contact {
+    public static Contact[] getAll() {
+        return new Contact[] {
+            new Contact("Malcolm Reynolds", "Captain", "123-456-7890"),
+            new Contact("Tom", "Doctor", "123-456-7300"),
+            new Contact("Rob", "Pirate", "123-456-7200"),
+            new Contact("Bob Doe", "Cop", "123-456-7100"),
+            new Contact("Tommy Malone", "Engineer", "123-456-7400")
+        };
+    }
+
+    public Contact(String name, String title, String phone) {
+        this.name = name;
+        this.title = title;
+        this.phone = phone;
+    }
+
+    private String name;
+    private String title;
+    private String email;
+    private String phone;
+    private String twitterId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTwitterId() {
+        return twitterId;
+    }
+
+    public void setTwitterId(String twitterId) {
+        this.twitterId = twitterId;
+    }
+}
---
+++ app/src/main/java/com/example/ggalasso/contactviewer_hw2_bpc/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.ggalasso.contactviewer_hw2_bpc;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setListAdapter(new ContactAdapter(this, R.layout.contact_item, Contact.getAll()));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This method will be called when an item in the list is selected.
+     * Subclasses should override. Subclasses can call
+     * getListView().getItemAtPosition(position) if they need to access the
+     * data associated with the selected item.
+     *
+     * @param l        The ListView where the click happened
+     * @param v        The view that was clicked within the ListView
+     * @param position The position of the view in the list
+     * @param id       The row id of the item that was clicked
+     */
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Contact contact = (Contact)getListAdapter().getItem(position);
+        Toast.makeText(this, "Clicked " + contact.getName() + " (" + id + ")", Toast.LENGTH_LONG).show();
+    }
+
+    class ContactAdapter extends ArrayAdapter<Contact> {
+
+        public ContactAdapter(Context context, int resource, Contact[] objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            if(convertView == null) {
+                view = getLayoutInflater().inflate(R.layout.contact_item, parent, false);
+            } else {
+                view = convertView;
+            }
+            Contact contact = getItem(position);
+            TextView nameView = (TextView)view.findViewById(R.id.item_name);
+            TextView titleView = (TextView)view.findViewById(R.id.item_title);
+            TextView phoneView = (TextView)view.findViewById(R.id.item_phone);
+
+            nameView.setText(contact.getName());
+            titleView.setText(contact.getTitle());
+            phoneView.setText(contact.getPhone());
+
+            return view;
+        }
+    }
+
+}
