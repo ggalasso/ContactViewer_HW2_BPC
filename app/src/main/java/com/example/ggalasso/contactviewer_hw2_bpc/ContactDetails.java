e815a7f569a01fd7960d134939b423d8434031dc
motwa004 <motwa004@umn.edu>
2015-02-22 11:01:59 -0600

    Actionbar icons displayed

--- app/src/main/java/com/example/ggalasso/contactviewer_hw2_bpc/ContactDetails.java
+++ app/src/main/java/com/example/ggalasso/contactviewer_hw2_bpc/ContactDetails.java
@@ -23,7 +23,7 @@ public class ContactDetails extends Activity {
 
        Intent intentExtras = getIntent();
         Bundle extrasBundle = intentExtras.getExtras();
-        if (! extrasBundle.isEmpty()) {
+        if (extrasBundle!=null && !extrasBundle.isEmpty()) {
             int id = extrasBundle.getInt("id");
             String contactName = extrasBundle.getString("name");
             String contactTitle = extrasBundle.getString("title");
