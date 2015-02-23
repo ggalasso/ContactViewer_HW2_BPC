cd724aff6eaf0a8fcf34e2e68c8f467ebebf5866
motwa004 <motwa004@umn.edu>
2015-02-22 22:08:31 -0600

    OnclickListener for Cancel and Save button on Add Contact Screen

--- app/src/main/java/com/example/ggalasso/contactviewer_hw2_bpc/AddContact.java
+++ app/src/main/java/com/example/ggalasso/contactviewer_hw2_bpc/AddContact.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class AddContact extends Activity {
@@ -12,6 +15,54 @@ public class AddContact extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        final EditText editFirstName = (EditText) findViewById(R.id.first_name_field);
+        final EditText editLastName = (EditText) findViewById(R.id.last_name_field);
+        final EditText editTitle = (EditText) findViewById(R.id.title_field);
+        final EditText editPhoneType = (EditText) findViewById(R.id.phone_field);
+        final EditText editPhoneNumber = (EditText) findViewById(R.id.phone_field2);
+        final EditText editEmailType = (EditText) findViewById(R.id.email_field);
+        final EditText editEmailAddress = (EditText) findViewById(R.id.email_field2);
+        final EditText editSocialType = (EditText) findViewById(R.id.social_field);
+        final EditText editSocial = (EditText) findViewById(R.id.social_field2);
+
+
+        final Button buttonSave = (Button) findViewById(R.id.Save);
+        final Button buttonCancel = (Button) findViewById(R.id.Cancel);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textFirstName = editFirstName.getText().toString();
+                String textLastName = editLastName.getText().toString();
+                String textTitle = editTitle.getText().toString();
+                String textPhoneType = editPhoneType.getText().toString();
+                String textPhoneNumber = editPhoneNumber.getText().toString();
+                String textEmailType = editEmailType.getText().toString();
+                String textEmailAddress = editEmailAddress.getText().toString();
+                String textSocialType = editSocialType.getText().toString();
+                String textSocial = editSocial.getText().toString();
+
+                }
+        });
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                editFirstName.setText("");
+                editLastName.setText("");
+                editTitle.setText("");
+                editPhoneType.setText("");
+                editPhoneNumber.setText("");
+                editEmailType.setText("");
+                editEmailAddress.setText("");
+                editSocialType.setText("");
+                editSocial.setText("");
+
+            }
+        });
     }
 
 
@@ -36,4 +87,6 @@ public class AddContact extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
